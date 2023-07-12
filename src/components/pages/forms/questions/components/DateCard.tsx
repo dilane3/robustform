ef5c6e911c6b10c0ab3464bbdd8f3ee3ccba1ecf
dilane3 <komboudilane125@gmail.com>
2023-07-12 21:00:58 +0100

    Handling edit form for date card

--- src/components/pages/forms/questions/components/DateCard.tsx
+++ src/components/pages/forms/questions/components/DateCard.tsx
@@ -1,10 +1,13 @@
 import Input from "@components/inputs/Input";
 import { Box, SxProps, Theme, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import { styles as cardStyles } from "@styles/mui-styles/form-card";
 import { DateTimePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import Card from "src/entities/card/Card";
+import { useActions, useSignal } from "@dilane3/gx";
+import { FormsState } from "src/gx/signals";
+import Question from "src/entities/card/Question";
 
 type DateCardProps = {
   card: Card;
@@ -15,6 +18,64 @@ export default function DateCard({
   card,
   onActive,
 }: DateCardProps) {
+  // Local state
+  const [label, setLabel] = React.useState(card.question.label || "");
+  const [subtitle, setSubtitle] = React.useState(card.subtitle || "");
+  const [modified, setModified] = React.useState(false);
+
+  // Global state
+  const { selectedFolder } = useSignal<FormsState>("forms");
+
+  // Global action
+  const { updateCard } = useActions("forms");
+
+  // Effects
+
+  useEffect(() => {
+    if (card.active) {
+      if (subtitle !== card.subtitle || label !== card.question.label) {
+        setModified(true);
+      } else {
+        setModified(false);
+      }
+    }
+  }, [label, subtitle]);
+
+  useEffect(() => {
+    if (!card.active && modified) {
+      // TODO: Update on supabase
+
+      let cardData = card.toOject();
+      let questionData = cardData.question;
+
+      questionData.label = label;
+      cardData.subtitle = subtitle;
+
+      const question = new Question(questionData);
+      const myCard = new Card({ ...cardData, question });
+
+      updateCard({
+        folderId: selectedFolder?.id,
+        formId: card.formId,
+        card: myCard,
+      });
+    } else {
+      console.log("dont save");
+    }
+  }, [card.active]);
+
+  // Handlers
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    target: string
+  ) => {
+    if (target === "label") {
+      setLabel(e.target.value);
+    } else if (target === "subtitle") {
+      setSubtitle(e.target.value);
+    }
+  };
+
   return (
     <Box
       sx={cardStyles.container}
@@ -32,6 +93,8 @@ export default function DateCard({
             label="Label"
             variant="standard"
             styles={{ marginBottom: 2 }}
+            value={label}
+            onChange={(e) => handleChange(e, "label")}
           />
 
           <Input
@@ -39,12 +102,14 @@ export default function DateCard({
             label="Subtitle"
             variant="standard"
             styles={{ marginBottom: 2 }}
+            value={subtitle}
+            onChange={(e) => handleChange(e, "subtitle")}
           />
         </Box>
       ) : (
         <Box sx={cardStyles.box}>
           <Typography component="h1" sx={cardStyles.label}>
-            Votre date de naissance
+            {card.question.label} 
           </Typography>
 
           <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -62,7 +127,7 @@ export default function DateCard({
           </LocalizationProvider>
 
           <Typography component="h5" sx={cardStyles.subtitle}>
-            Sous titre ici
+            {card.subtitle}
           </Typography>
         </Box>
       )}
