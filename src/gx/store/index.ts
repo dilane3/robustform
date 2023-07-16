d8172a1a2e44da620469d26b73bb024e215a4312
dilane3 <komboudilane125@gmail.com>
2023-07-16 22:22:39 +0100

    Setting up global state for authenticated user

--- src/gx/store/index.ts
+++ src/gx/store/index.ts
@@ -1,4 +1,5 @@
 import { createStore } from "@dilane3/gx";
 import { formsSignal, modalSignal } from "../signals";
+import { authSignal } from "../signals/auth";
 
-export const store = createStore([modalSignal, formsSignal]);
+export const store = createStore([modalSignal, formsSignal, authSignal]);
